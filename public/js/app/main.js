33c4ce8945e34cbffa25e1c4cca863d66526148e
Jason Miller <jasonmiller@synacor.com>
2014-01-16 19:39:21 -0500

    Sexy menu toggle

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -1,11 +1,25 @@
 define(['util', 'events', 'ford', 'app/routes'], function(util, events, $, routes) {
 	var app = events();
 	
-	// Set up routes:
-	app.routes = routes;
-	app.on('init', routes.init);
+	$.fn.toggleClass = function(className) {
+		$.iterate(this, function(c) {
+			c = $(c);
+			c[c.hasClass(className)?'declassify':'classify'](className);
+		});
+		return this;
+	};
 	
-	// Show active links:
+	app.on('init', function() {
+		// set up sidebar events
+		$('#menuLink, #menu a').on('click', function() {
+			// menu link is a toggle, sidebar links are close-only:
+			var op = this.id==='menuLink' ? 'toggleClass' : 'declassify';
+			$('#layout, #menu, #menuLink')[op]('active');
+		});
+	});
+	
+	// Set up routes:
+	app.on('init', routes.init);
 	routes.on('route', function(url) {
 		$('.pure-menu-selected').declassify('pure-menu-selected');
 		$('a[href="/#'+url+'"]').classify('pure-menu-selected');
