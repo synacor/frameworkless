79e7aa01a964aad67da55c46cbeea484dd48a06d
Jason Miller <jasonmiller@synacor.com>
2014-01-17 17:30:16 -0500

    Add docs.

--- demo-server.js
+++ demo-server.js
@@ -6,13 +6,15 @@ app.use(express.compress());
 
 // rewrite
 app.use(function(req, res, next) {
-	if (!req.url.match(/(^\/?static\/|\.[a-z]+$)/g)) {
+	if (!req.url.match(/(^\/?(static|docs)\/|\.[a-z]+$)/g)) {
 		req.url = '/';
 	}
 	next();
 });
 
-app.use(express.static('./'));
+app.use(express.static('demo/'));
+
+app.use('/docs', express.static('docs/'));
 
 app.listen(port, function() {
 	console.log('Server listening on localhost:'+port);
