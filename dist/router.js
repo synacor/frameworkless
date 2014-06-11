d0ab737037bea3451c82515a74afd17342af1778
Jason Miller <jasonmiller@synacor.com>
2014-06-11 19:41:02 -0400

    Dist for 0.1.3

--- dist/router.js
+++ dist/router.js
@@ -1,24 +1,24 @@
 /**	Instantiable declarative URL router.
  *	@module router
  *	@requires events
- *	
+ *
  *	@example
  *		<caption>Basic Usage:</caption>
- *		
+ *
  *		var router = require('router'),
  *			routes = router();
- *		
+ *
  *		// Define a route:
  *		routes.get('/pages/:name/:tab', function(params) {
  *			console.log(params.name, params.tab);
  *		});
- *		
+ *
  *		// Manually route to a page:
  *		routes.route('/pages/welcome/about');
  *
  *	@example
  *		<caption>Useful Tip: Automatically route to pages from HTML links.</caption>
- *		
+ *
  *		document.body.addEventListener('click', function(e) {
  *			var t=e.target, href;
  *			do {
@@ -39,7 +39,7 @@
 	}
 }(function(events) {
 	var EventEmitter = events.EventEmitter || events;
-	
+
 	/**	A URL router.
 	 *	@class module:router.Router
 	 *	@augments module:events.EventEmitter
@@ -47,71 +47,79 @@
 	 */
 	function Router() {
 		if (!(this instanceof Router)) return new Router();
-		
+
 		var router = this;
 		this.routes = [];
-		
+
 		if (!(this instanceof EventEmitter)) {
 			EventEmitter.call(this);
 		}
 		else if (events && typeof events.mixin==='function') {
 			events.mixin(this);
 		}
-		
+
 		addEventListener('popstate', function() {
-			route(router, location.pathname || location.hash);
+			route(router, location.pathname ? (location.pathname + location.search || '') : location.hash);
 		});
 	}
-	
+
 	try {
 		Router.prototype = new EventEmitter();
 		Router.prototype.constructor = Router;
 	} catch(err) {
 		console.warn(err.message);
 	}
-	
-	
+
+	/**	Initialize the router with the page's current URL,
+	 *	without adding a history entry and taking any baseUrl into account.
+     *	@param {String} [path=auto]		Override the routing path. Defaults to the page's URL.
+     *	@returns {Boolean} `true` if routing succeeded, `false` on error.
+     */
+    Router.prototype.init = function(path) {
+		return this.route(path || (location.pathname && (location.pathname + location.search)) || location.hash || '/', true, false);
+    };
+
 	/**	Use the given URL fragment as a prefix when parsing and creating all route URLs.
 	 *	@example
 	 *		router.setBaseUrl('/my-app/');
-	 *		
+	 *
 	 *		// routes are now relative to the Base URL:
 	 *		router.route('/hello', function() {
 	 *			console.log('Hello');
 	 *		});
-	 *		
+	 *
 	 *		// routing remains relative to the Base URL:
 	 *		router.route('/hello');		// "hello"
 	 */
 	Router.prototype.setBaseUrl = function(baseUrl) {
 		this.baseUrl = baseUrl;
 	};
-	
+
 	/**	A URL to which all routes are relative.  To change, use {@link module:router.Router#setBaseUrl setBaseUrl}.
 	 *	@string
 	 *	@default "/"
 	 */
 	Router.prototype.baseUrl = '/';
-	
-	
-	/**	Route to <code>url</code>, or register a <code>handler</code> to respond to <code>url</code>.  
-	 *	If <code>handler</code> is passed:  registers <code>handler</code> as a route for the given <code>url</code> pattern.  
+
+
+	/**	Route to <code>url</code>, or register a <code>handler</code> to respond to <code>url</code>.
+	 *	If <code>handler</code> is passed:  registers <code>handler</code> as a route for the given <code>url</code> pattern.
 	 *	If <code>handler</code> is omitted:  Attempts to route to the given <code>url</code>.
 	 *	@function module:router.Router#route
 	 *	@param {String} url							A URL to route to.
 	 *	@param {Function} [handler]					If given, <code>url</code> is instead used as a pattern that maps URLs to <code>handler</code>.
 	 *	@param {Boolean} [relativeToBaseUrl=true]	If <code>false</code>, the router will attempt to route to <code>url</code> without taking {@link module:router.Router#baseUrl baseUrl} into account.
-	 *	
+	 *
 	 *	@example
 	 *		<caption>Registering a route handler:</caption>
-	 *		
+	 *
 	 *		router.route('/jobs/:id', function(params) {
 	 *			console.log('Job ID: ' + params.id);
 	 *		});
-	 *	
+	 *
 	 *	@example
 	 *		<caption>Invoking a route: (uses previous example's route handler)</caption>
-	 *		
+	 *
 	 *		var didRoute = router.route('/jobs/1337');
 	 *		console.log( didRoute );   // true
 	 */
@@ -130,32 +138,42 @@
 			return this;
 		}
 		if (relativeToBaseUrl!==false && this.baseUrl) {
-			url = this.baseUrl.replace(/\/+$/g, '') + '/' + url.replace(/^\/+/g, '');
+			url = this.baseUrl.replace(/(?:^\/+|\/+(\?|$))/g, '$1') + '/' + url.replace(/^\/+/g, '');
 		}
 		history[handler===true ? 'replaceState' : 'pushState'](0, 0, url);
-		return this.currentUrl===url || route(this, url);
+		return this.currentUrlFull===url || route(this, url);
 	};
-	
+
 	/**	Alias of {@link module:router.Router#route}.
 	 *	@function module:router.Router#get
 	 */
 	Router.prototype.get = Router.prototype.route;
-	
-	
+
+
 	/**	Check if the given URL matches the current active route. */
 	Router.prototype.isUrlActive = function(url) {
 		var r = this.currentRoute;
 		return r && !!exec(url, r.url);
 	};
-	
-	
+
+
 	// Perform routing for the given router+url combo.
 	function route(router, url) {
-		var cur, matches, base, evt, i,
-			rawUrl = url,
+		var rawUrl = url,
 			old = router.currentRoute,
 			emit = router.emit || router.trigger,
-			handler;
+            query = {},
+			search, handler, cur, matches, base, evt, p, i;
+        matches = url.split('?');
+        if (matches.length>1) {
+            url = matches[0];
+            search = matches.slice(1).join('?');
+            p = search.split('&');
+            for (i=0; i<p.length; i++) {
+                matches = p[i].split('=');
+                query[ decodeURIComponent(matches[0]) ] = decodeURIComponent(matches.slice(1).join('='));
+            }
+        }
 		if (router.baseUrl) {
 			base = strip(router.baseUrl);
 			if (strip(url).substring(0, base.length)!==base) {
@@ -166,7 +184,9 @@
 		url = '/' + url.replace(/^\/+/g,'');
 		for (i=router.routes.length; i--; ) {
 			cur = router.routes[i];
-			matches = exec(url, cur.url);
+			if ( (matches = exec(url, cur.url)) ) {
+                matches.$query = query;
+            }
 			handler = cur.handler;
 			if (typeof handler==='object') {
 				if (router.currentRoute===cur && typeof handler.reload==='function') {
@@ -185,14 +205,17 @@
 					old.handler.unload();
 				}
 				router.currentUrl = url;
+				router.currentUrlFull = url + (search ? ('?' + search) : '');
 				router.currentUrlRaw = rawUrl;
 				router.currentRoute = cur;
-				
+
 				evt = {
 					url : url,
-					rawUrl : rawUrl
+					rawUrl : rawUrl,
+                    params : matches,
+                    query : query
 				};
-				
+
 				/**	Indicates the current "routed" URL has changed (a new route has become active).
 				 *	@event module:router.Router#route
 				 *	@type {Object}
@@ -210,7 +233,7 @@
 		}
 		return false;
 	}
-	
+
 	// Check if the given URL matches a route's URL pattern.
 	// @returns key-value matches for a match, or false for a mismatch
 	function exec(url, route) {
@@ -229,29 +252,29 @@
 		}
 		return matches;
 	}
-	
+
 	// Remove preceeding and trailing slashes from a URL
 	function strip(url) {
 		return url.replace(/(^\/+|\/+$)/g, '');
 	}
-	
+
 	// Get an Array containing the segments for a given URL
 	function segmentize(url) {
 		return strip(url).split('/');
 	}
-	
+
 	// Sort in descending order of number of real path segments
 	function sort(a, b) {
 		return segmentize(b.url).length - segmentize(a.url).length;
 	}
-	
-	
+
+
 	/**	If the module is called as a function, returns a new {@link router.Router} instance.
 	 *	@name router.router
 	 *	@function router.router
 	 */
-	
-	
+
+
 	Router.Router = Router;
 	return Router;
 }));
